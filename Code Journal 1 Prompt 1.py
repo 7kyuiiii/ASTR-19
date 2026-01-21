99dd38e538e0df5b72b93bcc0133c7a6693d2aa3
7kyuiiii <fanyuqi00@gmail.com>
2026-01-20 20:07:08 -0800

    Add files via upload

---
+++ Code Journal 1 Prompt 1.py
@@ -0,0 +1,7 @@
+
+print("Full Name: Yuqi")
+print("Preferred Pronouns: She/Her")
+
+print("My favorite movie is Flipped because it tells a heartfelt story about growing up.")
+print("My favorite food is chips because they are crunchy, salty.")
+
